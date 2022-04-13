1c654155df060f6f6f8a784bad3f866ee6aef0e2
Shambo Roy <67813512+Sam-byt@users.noreply.github.com>
2022-04-13 16:06:48 +0530

    Next Permutation

---
+++ Arrays/3ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        
+        int n = nums.size();
+        int index = -1;
+        
+        for(int i = n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                index = i;
+                break;
+            }
+        }
+        
+        if(index>=0){
+            for(int i= n-1; i>=0; i--){
+                if(nums[i]>nums[index]){
+                    swap(nums[i],nums[index]);
+                    break;
+                }
+            }
+        }
+        
+        index++;
+        reverse(nums.begin()+index,nums.end());
+        
+        
+        
+        
+        
+    }
+};
