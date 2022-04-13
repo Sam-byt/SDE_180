350093fbf97033d59ff731c5404faf048c51bb45
Shambo Roy <67813512+Sam-byt@users.noreply.github.com>
2022-04-13 16:09:55 +0530

    Maximum Subarray

---
+++ Arrays/4.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        
+        int sum = 0, maxsum = INT_MIN;
+        
+        for(auto i:nums){
+            sum += i;
+            maxsum = max(sum,maxsum);
+            if(sum<0)sum =0;
+        }
+        
+        return maxsum;
+    }
+};
