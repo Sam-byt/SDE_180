34bad89e62fee27d442a5c451317267c7215c995
Shambo Roy <67813512+Sam-byt@users.noreply.github.com>
2022-05-09 09:42:17 +0530

    M-Coloring Problem

---
+++ Recursion and Backtracking/M-Coloring.cpp
@@ -0,0 +1,69 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//Function to determine if graph can be coloured with at most M colours such
+//that no two adjacent vertices of graph are coloured with same colour.
+
+bool solve(int i,bool graph[101][101], int m , int n , vector<int>&color){
+    if(i == n)return true;
+    
+    for(int j =0; j<m;j++){
+        if(color[i]==-1){
+            bool flag = true;
+            for(int k=0;k<n;k++){
+                if(k!=i && graph[i][k] == 1){
+                    if(color[k] == j){
+                        flag = false;
+                        break;
+                    }
+                }
+            }
+            if(flag){
+                color[i] = j;
+                if(solve(i+1,graph,m,n,color) == true)return true;
+                else{
+                    color[i] = -1;
+                }
+            }
+        }
+    }
+    
+    return false;
+}
+
+
+
+bool graphColoring(bool graph[101][101], int m, int n)
+{
+    // your code here
+    vector<int>color(n,-1);
+    return solve(0,graph,m,n,color);
+}
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m, e;
+        cin >> n >> m >> e;
+        int i;
+        bool graph[101][101];
+        for (i = 0; i < n; i++) {
+            memset(graph[i], 0, sizeof(graph[i]));
+        }
+        for (i = 0; i < e; i++) {
+            int a, b;
+            cin >> a >> b;
+            graph[a - 1][b - 1] = 1;
+            graph[b - 1][a - 1] = 1;
+        }
+        cout << graphColoring(graph, m, n) << endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
